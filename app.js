01c9c1350d6e46e54b55d0e68c41b5c7b45ad90c
gary <gary.white@lodgit.com>
2022-02-15 16:49:15 +0100

    signup html added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var bodyParser = require('body-parser');
 var express = require('express');
-// var request = require('request');
+var request = require('request');
+
 const app = express();
-const port = 3000
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+app.listen(3000, function() {
+    console.log(`server is running on port 3000`);
+});
